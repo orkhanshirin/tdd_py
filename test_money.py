5994c17d68b4945d3d49ad459109bc46c797a49a
orkhanshirin <orkhan.shirinow@gmail.com>
2022-06-11 19:49:42 +0400

    Feat: 	Division and Multiplication tests added 	The corresponding features done

--- test_money.py
+++ test_money.py
@@ -7,17 +7,41 @@ from unittest import TestCase, main
 
 class TestMoney(TestCase):
     def test_multiplication(self):
-        fiver = Dollar(5)
-        tenner = fiver.times(2)
-        self.assertEquals(10, tenner.amount)
+        five_usd = Money(5, "USD")
+        ten_usd = Money(10, "USD")
+        self.assertEqual(ten_usd, five_usd.times(2))
+
+    def test_multiplication_eur(self):
+        ten_eur = Money(10, "EUR")
+        twenty_eur = ten_eur.times(2)
+        self.assertEqual(20, twenty_eur.amount)
+        self.assertEqual("EUR", twenty_eur.currency)
+
+    def test_division(self):
+        original_money = Money(4002, "KRW")
+        actual_money_after_division = original_money.divide(4)
+        expected_money_after_division = Money(1000.5, "KRW")
+        self.assertEqual(
+            expected_money_after_division.amount, actual_money_after_division.amount
+        )
+        self.assertEqual(
+            expected_money_after_division.currency, actual_money_after_division.currency
+        )
 
 
-class Dollar:
-    def __init__(self, amount):
+class Money:
+    def __init__(self, amount, currency):
         self.amount = amount
+        self.currency = currency
+
+    def __eq__(self, other):
+        return self.amount == other.amount and self.currency == other.currency
 
     def times(self, multiplier):
-        return Dollar(self.amount * multiplier)
+        return Money(self.amount * multiplier, self.currency)
+
+    def divide(self, divisor):
+        return Money(self.amount / divisor, self.currency)
 
 
 if __name__ == "__main__":
